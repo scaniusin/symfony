02ca7f76cd2f789661acea39067a30f16f9b3ae3
scaniusin <scaniusin@gmail.com>
2018-03-31 11:28:09 +0300

    db

---
+++ dump_db/dump-symfonyreact-201803311124.sql
@@ -0,0 +1,124 @@
+-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
+--
+-- Host: localhost    Database: symfonyreact
+-- ------------------------------------------------------
+-- Server version	5.7.21-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `blog_post`
+--
+
+DROP TABLE IF EXISTS `blog_post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blog_post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `body` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=223 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blog_post`
+--
+
+LOCK TABLES `blog_post` WRITE;
+/*!40000 ALTER TABLE `blog_post` DISABLE KEYS */;
+INSERT INTO `blog_post` VALUES (189,'Excepturi nihil molestias eveniet nihil ducimus ut.','Consequatur iure tempore dolor voluptatem optio id quis. Fuga corporis nihil impedit sint veritatis. Sequi et numquam distinctio commodi autem tempore.'),(190,'new title1','new1'),(192,'Est et quo consequatur aut et at enim.','Quaerat ipsam est voluptate et. Consectetur tempora deleniti consequuntur autem hic. Amet ea voluptates est quis.'),(193,'Est itaque unde id nisi consequatur dolorem consequatur.','Explicabo dolores ullam sed. Saepe placeat earum ea dolore a eum. Quam ut sunt quia cumque aperiam quia. Corrupti nisi laboriosam ducimus tempore optio quo voluptas ea.'),(194,'Veniam impedit corrupti nemo officia eos.','Et nobis rerum ut voluptatem quia consectetur itaque. Ut unde qui qui et omnis ratione quibusdam. Nemo autem harum corrupti delectus. Quo voluptatibus voluptatem aut rem corrupti quisquam.'),(195,'Aut suscipit culpa illo aut molestias.','Voluptatum voluptas in deleniti molestias. Animi eos dolor vero voluptatum eius quia itaque. Qui delectus accusantium porro aperiam eum.'),(196,'Et aspernatur est excepturi vitae ut dolor ipsam ratione.','Explicabo tempore iste ullam ipsa quia. Id temporibus ad provident sed officia soluta. Dolor ipsum sit placeat autem qui hic asperiores officia. Aut et quia quae ipsum.'),(197,'Non facilis vel laborum quasi non inventore laborum tempore.','Qui ipsam laudantium architecto fugit. A animi error deserunt beatae.\nId ut nesciunt amet inventore est. Et tempora eligendi sequi voluptatem. Non totam ut quia quo alias iure consequuntur.'),(198,'Eos repudiandae quia est praesentium ducimus aspernatur nihil.','Sint consequatur neque ipsa suscipit tempore quia. Architecto accusamus repellendus consequatur aut rem. Aut totam aut quia et.'),(199,'Et necessitatibus ad earum id minima.','Esse eius dolorem perferendis sunt. Nobis dolor praesentium sit et dicta. Iusto nostrum eos quis eos magni. Voluptatem ut facere aut consectetur autem quia.'),(200,'Magnam molestiae eaque quia repudiandae vel neque.','Fugit quas ut consequatur deserunt dolores. Voluptatem ab optio sunt. Voluptatibus tempore assumenda eveniet autem est dignissimos.'),(201,'Quia enim optio est consectetur non.','Blanditiis nemo in eos error eligendi fugiat. Ut vero aspernatur aut nihil qui odio. Est consequuntur fugit officiis rerum ipsa ad. Possimus qui aut maxime itaque.'),(202,'Quis nam eos quos labore ipsam excepturi qui.','Adipisci nihil aliquid at. Rerum rerum voluptate hic cumque dolor. Omnis autem dolorum eligendi qui.'),(203,'Quos quas voluptas laudantium.','Provident molestias dolores nostrum pariatur quas. Soluta quasi placeat modi eos ipsum quam. Quaerat corrupti vel ipsam. Voluptatibus ad quis quia est repellat et explicabo.'),(204,'Quas assumenda ipsum rerum perferendis deleniti veniam nisi.','Officiis rerum ut maxime dolorum. Id laboriosam vitae tenetur iusto. Necessitatibus fugiat unde architecto quia autem dolorum. Quae consequuntur rerum repudiandae amet enim.'),(205,'Est omnis maxime nisi itaque et.','Vitae nobis harum molestias velit omnis voluptatem et. Doloremque eum rerum quo magnam ea. Id quod unde modi. Possimus a perspiciatis inventore dolores optio quia.'),(206,'Vel autem sit temporibus ipsa.','Maiores qui est animi. Aut iste consequatur quas et laboriosam.'),(207,'Quis ut veritatis cum sit velit dolorem molestiae.','Ea facere assumenda nobis aperiam. Eaque beatae aperiam autem molestias corporis voluptatem fuga.'),(208,'Mollitia aut nihil expedita vel.','Non rerum aut asperiores repellat. Sunt aut maiores cumque et nihil voluptas incidunt. Animi est quibusdam accusantium eos fuga debitis. Vel iure et quis at ut sit aut.'),(209,'Dolorem sit vel ex sed ipsum exercitationem.','Temporibus ea odio harum et consequatur corporis est. Ut quia eveniet aperiam praesentium molestiae dolor enim officia.'),(210,'Est veniam dolore consequuntur architecto vel eum molestiae.','Perferendis voluptas debitis aut qui quas magnam rerum. Beatae qui et aut officiis provident accusantium ducimus culpa. Assumenda quod maxime dolorem eaque iure aliquid.'),(211,'Aspernatur nemo omnis tempore asperiores iste.','Doloremque eligendi ex harum id. Nisi expedita assumenda ut reiciendis in et culpa odit. Consequuntur rerum suscipit ut ut. Unde eius voluptatem eveniet iste qui id.'),(212,'Ipsam similique ipsum esse voluptatem consectetur eos.','Qui expedita voluptate sit provident. Corrupti omnis amet aut eum. Velit sunt molestiae tempore consequatur natus optio perspiciatis. Ab quidem suscipit dicta et cum.'),(213,'Deleniti facilis assumenda exercitationem voluptas.','Consequuntur nam incidunt in ullam doloremque perferendis. Sequi rerum illo ratione earum cupiditate suscipit iusto. Harum sit voluptate est doloribus.'),(214,'Delectus dolorum quod consequatur.','Molestiae et non qui ut tenetur reprehenderit. Nostrum et sit reiciendis qui. Tempore id dolorem numquam laborum necessitatibus aliquam eius. Voluptatem quia neque distinctio veritatis atque qui.'),(215,'Quo delectus nesciunt doloremque maiores consequatur.','Itaque fugiat sequi aperiam nesciunt officiis. Optio autem magnam aliquid laborum qui pariatur. Harum autem eos enim at soluta rerum minima et.'),(216,'Ullam maxime impedit voluptates.','Ipsa quasi error voluptas autem. Porro ad temporibus et aliquam corrupti. Beatae odio eveniet mollitia placeat quod maxime. Modi voluptas similique expedita consequuntur dolor dignissimos.'),(217,'Dolorum aut repellendus et ipsum delectus velit.','Molestias ut voluptas et aut et. Quasi veniam itaque magnam aut nobis eveniet voluptatem. Quia vitae dolores ut. Magnam ea omnis dolorem perspiciatis delectus sunt.'),(218,'Delectus quis nihil veritatis odit ut repudiandae provident.','Fugit omnis veniam voluptates facilis aliquid. Itaque sint et nemo quis. Tenetur qui aut voluptatem nemo. Voluptatibus vitae ut non eligendi esse.'),(219,'Voluptatibus recusandae voluptatem culpa sequi.','Velit non repudiandae perferendis nihil ab officia facere nihil. Facilis laborum consequatur tempora voluptatem. Illum nostrum expedita ea. Dicta deserunt dolorem mollitia distinctio eveniet quos.'),(220,'A facere eveniet architecto ex eos nobis minus.','Dolor quis cum laboriosam. Est aut tempora aut. Soluta accusamus atque pariatur voluptatem.'),(221,'Deleniti aut soluta maiores error nihil sint.','Ex consectetur tempore autem quidem at. Dignissimos saepe aut in voluptatum nihil natus temporibus. Qui aliquam culpa quibusdam sed excepturi et voluptatum.'),(222,'Sapiente tempora quia et facilis.','Sit tempore ea excepturi est eligendi eligendi saepe. Et saepe officia aperiam eveniet. Autem sed adipisci pariatur fugiat. Praesentium voluptatem non error eos quidem omnis similique non.');
+/*!40000 ALTER TABLE `blog_post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fos_user`
+--
+
+DROP TABLE IF EXISTS `fos_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fos_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `username_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `email_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `salt` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `locked` tinyint(1) NOT NULL,
+  `expired` tinyint(1) NOT NULL,
+  `expires_at` datetime DEFAULT NULL,
+  `confirmation_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password_requested_at` datetime DEFAULT NULL,
+  `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
+  `credentials_expired` tinyint(1) NOT NULL,
+  `credentials_expire_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_957A647992FC23A8` (`username_canonical`),
+  UNIQUE KEY `UNIQ_957A6479A0D96FBF` (`email_canonical`),
+  UNIQUE KEY `UNIQ_957A6479C05FB297` (`confirmation_token`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fos_user`
+--
+
+LOCK TABLES `fos_user` WRITE;
+/*!40000 ALTER TABLE `fos_user` DISABLE KEYS */;
+INSERT INTO `fos_user` VALUES (1,'peter','peter','peter@test.com','peter@test.com',1,'2p2v3751wbswok4g0gkgck4ssco4g48','$2y$13$Zv54r8dMgSgBSRkgDdwHfuJz7UdAlPSA5m9OYnbtYan6TD6VspGza','2018-03-15 12:46:08',0,0,NULL,NULL,NULL,'a:0:{}',0,NULL),(2,'peter1','peter1','asd@asd.sd','asd@asd.sd',1,'36ad44o07bi80coswgcswwg4o4gs8kw','$2y$13$EuWJLQnVQ5iJtrW2.xc3VuT7g/cqveG47Qu9X2R2jQ9r3w.8p8pH2','2018-02-09 01:58:01',0,0,NULL,NULL,NULL,'a:0:{}',0,NULL),(3,'serj2','serj2','scaniusin@yahoo.com','scaniusin@yahoo.com',1,'dhqk1tbw38ooo44w8cos8g80sck0cs4','$2y$13$qCqxRGtxhWklHK1gTwgjuO8KXooP0AAbuvGfHVZOczK5WaE9ytc5y','2018-02-11 09:55:25',0,0,NULL,NULL,NULL,'a:0:{}',0,NULL),(4,'serj3','serj3','serj@mail.ru','serj@mail.ru',1,'hy942s2xdvk0o0s8s0ocgo4osww444g','$2y$13$T5f23PUq1KmHceX1VFIsOeZovfu1s0BVD5whXTQ/2BBTqM3el0NVi','2018-02-11 09:55:47',0,0,NULL,NULL,NULL,'a:0:{}',0,NULL),(5,'serj4','serj4','scani2usin@yahoo.com','scani2usin@yahoo.com',1,'hari7vvqkjs4gswwkcgcgww80g4s4gg','$2y$13$SMXcQxPD6uT/OpAr/Zrlx.6eTareYaAzYLi1MyJlsPHvS8tr0ezvC','2018-02-11 09:56:19',0,0,NULL,NULL,NULL,'a:0:{}',0,NULL),(6,'serj43','serj43','scaniusins@yahoo.coms','scaniusins@yahoo.coms',1,'mq0iqh7hylwcs80s044s4g0gk84kkcw','$2y$13$4I.X7xRo9B2ixXDNjqIPlOYrIO6zIaCO9DsIk88gvFDuCtYH4IF22','2018-02-23 09:24:03',0,0,NULL,NULL,NULL,'a:0:{}',0,NULL),(7,'asd','asd','asd@asd.sdss','asd@asd.sdss',1,'tufb6k73ulw880wwswcokcwkskgo80w','$2y$13$d1pPUbTIYIuMvfZBgVY9fuKD6v.B9bKunvBetgzngE2mSwXo7WP7q','2018-02-23 09:25:10',0,0,NULL,NULL,NULL,'a:0:{}',0,NULL),(8,'serjsss','serjsss','scaniusin@yahoo.comsss','scaniusin@yahoo.comsss',1,'ge9ygyd9kf4kgwwkwwc4ko8w484oks8','$2y$13$EcBpKe/zvUbqUb8WF0pZW.R3lpQOKVxiuxUWDxlBAyuR95nRXUSmq','2018-02-23 10:34:44',0,0,NULL,NULL,NULL,'a:0:{}',0,NULL),(9,'dsa','dsa','asdas@asd.sds','asdas@asd.sds',1,'byijk5l3bcowswo8oocow0gggg0880g','$2y$13$Gw2./BmMzhWyv1sQ/PLD2ecs19muYvyJ7Ee.0J1yAAdYBoZiwbd7K','2018-03-09 01:40:39',0,0,NULL,NULL,NULL,'a:0:{}',0,NULL),(10,'qwe','qwe','qwe@qwe.qwe','qwe@qwe.qwe',1,'jztgty073lwk8gco0c0gs4wwowosw8g','$2y$13$4SlLlclpCuYnyIp/WCldSuFBPmypyTL2JfxgG8PwQskRHidqKtON2','2018-03-15 11:21:04',0,0,NULL,NULL,NULL,'a:0:{}',0,NULL),(11,'zxc','zxc','zxc@zxc.zxc','zxc@zxc.zxc',1,'mjeev5y14c0ow04occcwggg8wgw48kc','$2y$13$RjtS4.lwezrTzeRnANdRcecKTXm34NKP9zfwfiDmfa7WiT8yfTUO.','2018-03-15 12:45:37',0,0,NULL,NULL,NULL,'a:0:{}',0,NULL);
+/*!40000 ALTER TABLE `fos_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `wallet`
+--
+
+DROP TABLE IF EXISTS `wallet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wallet` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `public_address` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_7C68921FA76ED395` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `wallet`
+--
+
+LOCK TABLES `wallet` WRITE;
+/*!40000 ALTER TABLE `wallet` DISABLE KEYS */;
+INSERT INTO `wallet` VALUES (1,2,'new1'),(2,9,'new1'),(4,11,'04995e003ff9bcc479cd4e9a80a2fbd8624225c80b873e438d6b203d00d2f4a48550c2e0ae3ae1116648e71e199602b4e313351a1b51e3eda2e43c6eef74085a4e'),(6,10,'04fdcf7708a88a105ec032a75d6d8c87324a445088b6f6441c5aff752cada295d67d09d3c6bcb30417b9606464705e89a74bfb6c772ef3d4f1baf58e5f0df35e3e'),(7,12,'04b77eb3f267f56cdb88d1debcb2dfa1f8b73e7e03e93a26093f5804e76c235549cc0dac5499b80cdf3fd08e3f63bbfe19f9306df0ed89e8113727eaa5085f5aa5'),(9,1222,'04ed33a7b53f42b211d0269e291f8f1dc74265de2146968582ef33a0ec470822c051bd591cd2ce9d54ab1d8cca6d28cbae507e09d149ca1ee20249b3d355e7a12f'),(10,23,'048cdde14b275f5394f95802000c06128e145847c616b127ac5fee542b43aa860c7636bd77edd14ca2ae2a8f3bf019ac31592dda06e54ad5a936fc35ed3d919e06'),(11,1,'04163e189cb511052bc7725e838b3872edb55c7915711e8975569ce0465ab09a2162442543723769873ccf2394c34c8564864dbe76fcc91c5f38591bb5d00af22f');
+/*!40000 ALTER TABLE `wallet` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'symfonyreact'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-31 11:24:19
